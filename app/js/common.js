6861338e184ff2e380783cc56ca8c5dc0541a43d
novDmitry <ndmitiry@gmail.com>
2018-10-09 21:05:33 +0300

    NovaBoard 1.0

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,21 @@
 $(function() {
 
-	// Custom JS
+    var swiper = new Swiper('.swiper-container', {
+        spaceBetween: 10,
+        slidesPerView: 1,
+        centeredSlides: true,
+        autoplay: {
+            delay: 4500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 
 });
